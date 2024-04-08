fa655d0a21e2823f4a27e8ff2a18de09fe970b86
askolds <askolds11@gmail.com>
2024-04-08 23:22:28 +0300

    activity10

--- src/main/java/jtm/activity10/StreamEditor.java
+++ src/main/java/jtm/activity10/StreamEditor.java
@@ -1,10 +1,6 @@
 package jtm.activity10;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 
 /*-
  * This is simple text stream editor. It reads text from file/standard input,
@@ -36,7 +32,7 @@ public class StreamEditor {
 	public static void main(String[] args) throws Exception {
 		String inFileName, outFileName; // Names of input and output files
 		int inLineNo = 0; // Line number, which needs to be changed, deleted
-		String content; // String content which needs to be put in line
+		String content = null; // String content which needs to be put in line
 		PrintWriter writer = null; // Buffered writer of characters
 		BufferedReader reader = null; // Buffered reader of characters
 		File inFile; // File for file system operations
@@ -44,41 +40,96 @@ public class StreamEditor {
 		String curLineContent; // Content of current line of input file
 		boolean delete = false; // Should delete line?
 
-		/*- TODO Check number of passed parameters. If they are null or number of
+		/*- Check number of passed parameters. If they are null or number of
 		 * them is not 4, write to standard error (System.err):
 		 * Please use arguments: [-]lineNo (TextToAdd/Replace|-) (inputFile|-) (outputFile|-)
 		 * and exit with System.exit(1); to pass error status of finished program.
 		 */
+		if (args == null || args.length != 4) {
+			System.err.println("Please use arguments: [-]lineNo (TextToAdd/Replace|-) (inputFile|-) (outputFile|-)");
+			System.exit(1);
+		}
 
-		// TODO Get integer from the 1st argument. Note that line should be
+		// Get integer from the 1st argument. Note that line should be
 		// deleted if number is negative.
 		// Hint. Use Integer.parseInt() to parse String into integer
+		inLineNo = Integer.parseInt(args[0]);
+		if (inLineNo < 0) {
+			inLineNo = -inLineNo;
+			delete = true;
+		}
 
-		// TODO set value of the string from 2nd parameter into content
+		// set value of the string from 2nd parameter into content
+		if (!args[1].equals("-")) {
+			content = args[1];
+		}
 
-		/*- TODO Initialize new buffered character reader (BufferedReader) and:
+		/*- Initialize new buffered character reader (BufferedReader) and:
 		 * 1. If input file name (3rd parameter) is "-", add reader to the Standard input (System.in).
 		 * 2. Otherwise check if file exists (if it doesn't, create it) and 
 		 *    add reader to this file.
 		 */
+		inFileName = args[2];
+		if (inFileName.equals("-")) {
+			reader = new BufferedReader(new InputStreamReader(System.in));
+		} else {
+			inFile = new File(inFileName);
+			if (!inFile.exists()) {
+				inFile.createNewFile();
+			}
+			reader = new BufferedReader(new FileReader(inFile));
+		}
 
-		/*- TODO Initialize new buffered character writer (PrintWriter) and:
+		/*- Initialize new buffered character writer (PrintWriter) and:
 		 *  1. If output file name (4th parameter) is "-", add writer to the standard output (System.out)
 		 *  2. Otherwise initialize writer to the file of given name.
 		 */
+		outFileName = args[3];
+		writer = outFileName.equals("-")
+				? new PrintWriter(System.out)
+				: new PrintWriter(new FileWriter(outFileName));
 
-
-		// TODO Read lines in loop from passed file/standard input till to the
+		// Read lines in loop from passed file/standard input till to the
 		// end. Count number of read lines. Before appending line into writer
 		// check, if it needs to be changed/deleted. Change its value to passed
 		// content or just skip appending it to the writer.
 		// NOTE: append break at the end of written line only if it is NOT null
 		// or empty string!
+		while ((curLineContent = reader.readLine()) != null) {
+			curLineNo++;
 
-		// TODO If number of input line is larger than number of lines in file,
+			if (inLineNo == 0 || curLineNo != inLineNo) {
+				writer.println(curLineContent);
+				continue;
+			}
+
+			// if line needs to be deleted, do not write the current line
+			if (delete) {
+				continue;
+			}
+
+			// line needs to be modified
+			if (content != null && !content.isEmpty()) {
+				writer.println(content);
+			}
+		}
+
+
+		// If number of input line is larger than number of lines in file,
 		// pad file with empty lines before necessary line.
+		if (!delete && inLineNo > curLineNo) {
+			while (curLineNo < inLineNo - 1) {
+				writer.println();
+				curLineNo++;
+			}
+			writer.println(content);
+		}
 
-		// TODO flush cache of the writer and close connections of the reader
+
+		//  flush cache of the writer and close connections of the reader
 		// and writer
+		writer.flush();
+		writer.close();
+		reader.close();
 	}
 }
