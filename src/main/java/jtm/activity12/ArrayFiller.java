f65ddece7f113fbe12a5bdc6ac5752b99b04567c
askolds <askolds11@gmail.com>
2024-06-02 05:45:26 +0300

    activity12

--- src/main/java/jtm/activity12/ArrayFiller.java
+++ src/main/java/jtm/activity12/ArrayFiller.java
@@ -2,8 +2,6 @@ package jtm.activity12;
 
 import java.util.Random;
 
-import static jtm.activity12.ArrayFillerManager.array;
-
 public class ArrayFiller implements Runnable {
 
 	int latency; // required latency time (in miliseconds) to simulate real
@@ -13,22 +11,35 @@ public class ArrayFiller implements Runnable {
 	Random random; // Pseudo-random generator
 
 	public ArrayFiller(int latency, int startValue) {
-		// TODO from this constructor call another constructor with more
+		// from this constructor call another constructor with more
 		// parameters and fill missing values from beginning till to the end of an array
+		this(latency, startValue, 0, ArrayFillerManager.array.length - 1);
 	}
 
 	public ArrayFiller(int latency, int startValue, int from, int to) {
-		// TODO save passed values to created filler object
+		// save passed values to created filler object
+		this.latency = latency;
+		this.startValue = startValue;
+		this.from = from;
+		this.to = to;
 	}
 
 	@Override
 	public void run() {
-		// TODO when invoked, put filler to sleep for required amount of latency using
+		// when invoked, put filler to sleep for required amount of latency using
 		// Thread.sleep(latency) method
-		// TODO fill ArrayFillerManager.array from..to cells with pseudo random values
+        try {
+            Thread.sleep(latency);
+        } catch (InterruptedException e) {
+        }
+        // fill ArrayFillerManager.array from..to cells with pseudo random values
 		// initialize new Random(startValue + arrayCellNo) and then use
 		// random.nextInt() to get value for given cell
 		// Look at http://docs.oracle.com/javase/7/docs/api/java/util/Random.html
+		for (int i = from; i <= to; i++) {
+			random = new Random(startValue + i);
+			ArrayFillerManager.array[i] = random.nextInt();
+		}
 	}
 
 }
--- src/main/java/jtm/activity12/ArrayFillerManager.java
+++ src/main/java/jtm/activity12/ArrayFillerManager.java
@@ -25,33 +25,43 @@ public class ArrayFillerManager {
 	 * @return
 	 */
 	public static int[] setUp(int arraySize, int latency, int startValue) {
-		// TODO save passed values in prepared structure
+		// save passed values in prepared structure
 		// initialize array with passed size
 		// initialize list of threads
 		// return reference to the initialized array
-		return null;
+		ArrayFillerManager.array = new int[arraySize];
+		ArrayFillerManager.latency = latency;
+		ArrayFillerManager.startValue = startValue;
+		threads = new LinkedList<>();
+		return array;
 	}
 
 	public static void fillStupidly() {
-		// TODO create cycle which creates new ArrayFiller objects
+		// create cycle which creates new ArrayFiller objects
 		// with filling range of only ONE cell at a time (i.e. range from..to is
 		// 0..0, then 1..1, etc.) and invoke .run() method for these objects.
 		// Note, that invocation of .run() method directly executes it in
 		// current (main) thread.
 		// Note that this method emulates, what would happen if you would send
 		// just small portions of data through media with latency.
+		for (int i = 0; i < array.length; i++) {
+			ArrayFiller arrayFiller = new ArrayFiller(latency, startValue, i, i);
+			arrayFiller.run();
+		}
 	}
 
 	public static void fillSequentially() {
-		// TODO create one ArrayFiller object with filling range for ALL array but
+		// create one ArrayFiller object with filling range for ALL array but
 		// executed just in SINGLE thread by invocation of .run() method directly.
 		// Note that this method emulates, what would happen if you would do
 		// proper "buffering" with large amount of data, but do execution just
 		// in single thread.
+		ArrayFiller arrayFiller = new ArrayFiller(latency, startValue, 0, array.length - 1);
+		arrayFiller.run();
 	}
 
 	public static void fillParalelly() {
-		// TODO create cycle which creates new ArrayFiller objects
+		// create cycle which creates new ArrayFiller objects
 		// with any range and pass them as references to the Thread constructor.
 		// Add newly created Thread objects into threads list and start them
 		// threads using .start() method. Note that invocation of .start() for
@@ -63,7 +73,27 @@ public class ArrayFillerManager {
 		// actually finished by calling .join() method for them.
 		// Note that this method emulates, what would happen if you do proper
 		// buffering and scaling of the execution.
-
+		ArrayFiller arrayFiller;
+		int threadCount = 4;
+		int arraySize = array.length;
+		int valuesPerThread = arraySize / threadCount;
+		for (int currentThread = 0; currentThread < threadCount; currentThread++) {
+			int from = currentThread * valuesPerThread;
+			int to = (currentThread + 1) * valuesPerThread - 1;
+			to = Math.min(to, array.length);
+			arrayFiller = new ArrayFiller(latency, startValue, from, to);
+			Thread thread = new Thread(arrayFiller);
+			threads.add(thread);
+			thread.start();
+		}
+		for (Thread thread : threads) {
+            try {
+				if (thread != null) {
+					thread.join();
+				}
+            } catch (InterruptedException e) {
+            }
+        }
 	}
 
 }
